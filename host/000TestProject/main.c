68e3b8453453d66d90854a0918adc4fdc9a39744
quanngo150205 <quan.ngo150205@hcmut.edu.vn>
2026-04-24 15:28:43 +0700

    init project for the course

---
+++ host/000TestProject/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ *
+ *  Created on: Apr 24, 2026
+ *      Author: ngoqu
+ */
+#include <stdio.h>
+
+int main (void) {
+
+	printf ("Hello world\n");
+
+	printf ("Press 'enter' to exit this application\n");
+
+	getchar();
+
+	return 0;
+
+}
+
